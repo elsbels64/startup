eda6146ed6b196dadce65ce27defe970a117c96b
Elise <ewwirthlin@gmail.com>
2025-02-18 19:12:31 -0700

    card is flipping to a new card

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './game.css';
 
 export function Game() {
+
+  const defaultCards = [
+    { image: "https://deckofcardsapi.com/static/img/AS.png", code: "AS" },
+    { image: "https://deckofcardsapi.com/static/img/KD.png", code: "KD" },
+    { image: "https://deckofcardsapi.com/static/img/QH.png", code: "QH" }
+  ];
+
+  const [card, setCard] = useState(defaultCards[0]);
+  const [fallbackIndex, setFallbackIndex] = useState(0);
+
+  const flipCard = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1");
+      const data = await response.json();
+      if (data.cards && data.cards.length > 0) {
+        setCard({ image: data.cards[0].image, code: data.cards[0].code });
+      } else {
+        throw new Error("No cards received from API");
+      }
+    } catch (error) {
+      console.error("API request failed, using fallback card", error);
+      setFallbackIndex((prevIndex) => (prevIndex + 1) % defaultCards.length);
+      setCard(defaultCards[fallbackIndex]);
+    }
+  };
+
   return (
     <main>
       <div className = "container">
@@ -21,14 +48,14 @@ export function Game() {
         <div className = "game-display">
           <div className = "fliped-card-container">
             <div className = "card-front">
-              <img src = "https://deckofcardsapi.com/static/img/AS.png" alt = "ace of spades"></img>
+              <img src={card.image} alt={`Card: ${card.code}`} />
             </div>
             <div className = "card-back">
               <img src = "https://deckofcardsapi.com/static/img/back.png" alt = "the back of a card"></img>
             </div>
           </div>
         </div>
-        <form id="prediction-form">
+        <form id="prediction-form" onSubmit={flipCard}>
           <fieldset>
             <legend>Your Prediction</legend>
             <input type="radio" className = "btn-check" id="higher" name="varRadio" value="higher" autoComplete="off" required/>
