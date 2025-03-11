8499dee2785ff1b94e54c9906ebc87acc263f3b6
Elise <ewwirthlin@gmail.com>
2025-03-11 15:58:05 -0600

    wrote logout calls

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -4,15 +4,34 @@ import { NavLink } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
 
+import { MessageDialog } from './messageDialog';
+
+
 export function Authenticated(props) {
-  function logout() {
-    localStorage.removeItem('username');
-    props.onLogout();
+  const [error, setError] = React.useState(null);
+
+  async function logout() {
+    try{
+      const response = await fetch("/api/auth/logout", {
+      method: "delete",
+      })
+      if(response?.status === 200) {
+        localStorage.removeItem('username');
+        props.onLogout();
+      } else {
+        const body = await response.json();
+        setError(`Something Went Wrong: ${body.msg}`);
+      }
+  }catch (error) {
+    //Network failure or server is completely unreachable
+    setError("Request failed: The server is unreachable or down.");
   }
+}
 
   const navigate = useNavigate();
 
   return (
+    <>
     <div>
       <div className='playerName'>{props.username}</div>
       <Button variant='primary' onClick={() => navigate('/game')}>
@@ -22,5 +41,7 @@ export function Authenticated(props) {
         Logout
       </Button>
     </div>
+    <MessageDialog message={error} onHide={() => setError(null)} />
+    </>
   );
 }
