a735cf762c9045655a3d47382757edd80b8b5bef
Elise <ewwirthlin@gmail.com>
2025-03-27 12:54:32 -0600

    cleaned up scores

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -8,12 +8,8 @@ export function Scores() {
     .then((response) => response.json())
     .then((highScores) => {
       setScores(highScores);
-      // console.log(highScores);
-      // console.log(scores);
     })
   }, []);
-
-  //console.log(scores)
 
   return (
     <main>
